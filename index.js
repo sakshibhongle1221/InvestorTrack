95f6a4bf4b08e0a0a405c18b5c03bf9400c352f8
sakshibhongle1221 <sakshibhongle1221@gmail.com>
2025-08-27 12:28:56 +0530

    updated code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const db = require('./config/db');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const userRoutes = require('./routes/userRoutes');
 const transactionRoutes = require('./routes/transactionRoutes');
 const budgetRoutes = require('./routes/budgetRoutes');
